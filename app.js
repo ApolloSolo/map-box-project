44eabda19f5c69bc4a2d939fcacf589f8bc4369f
ApolloSolo <matt1teixeira@gmail.com>
2022-03-03 20:30:14 -0500

    Loop through coords and place pins on map

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const mapKey = "pk.eyJ1IjoiYXBwc29sbyIsImEiOiJjbDA5dmptYWowaGcwM2lwOTY0dGxlOWp3In0.kulAfdlLVedrwX0Yh0qruQ";
 
 const cities = ["Charlotte", "Sacramento", "Gustine", "Ashboro"]
-let geoJSON = [];
+let cityGeoJSON = [];
 
 const cityToGeoData = async function (city) {
     const respons = await axios.get(
@@ -10,7 +10,7 @@ const cityToGeoData = async function (city) {
     if (respons) {
       const lat = respons.data[0].lat;
       const lon = respons.data[0].lon;
-      const geoData = [lat, lon];
+      const geoData = [lon, lat];
       return geoData;
     } else {
       alert("Error with geo location");
@@ -20,157 +20,34 @@ const cityToGeoData = async function (city) {
 const toGeoJSON = async function() {
     for(let i = 0; i < cities.length; i++){
         let data = await cityToGeoData(cities[i]);
-        let location_i = {
-            type: "Point", 
-            coordinates: [data]
+        
+        cityGeoJSON.push(data);
+        console.log(data);
+
+        if(i === cities.length - 1){
+          buildMap();
         }
-        JSON.stringify(location_i);
-        geoJSON.push(location_i);
     }
-    console.log(geoJSON);
+    console.log(cityGeoJSON);
 }
 
 toGeoJSON();
 
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiYXBwc29sbyIsImEiOiJjbDA5dmptYWowaGcwM2lwOTY0dGxlOWp3In0.kulAfdlLVedrwX0Yh0qruQ';
+function buildMap(){
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYXBwc29sbyIsImEiOiJjbDA5dmptYWowaGcwM2lwOTY0dGxlOWp3In0.kulAfdlLVedrwX0Yh0qruQ';
 
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/dark-v10', // style URL
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
     center: [-80.9, 35.2], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    zoom: 5 // starting zoom
 });
 
-// // Create a default Marker and add it to the map.
-// const marker1 = new mapboxgl.Marker()
-// .setLngLat([-80.9, 35.2])
-// .addTo(map);
- 
-// // Create a default Marker, colored black, rotated 45 degrees.
-// const marker2 = new mapboxgl.Marker({ color: 'black', rotation: 45 })
-// .setLngLat([-80.9, 35.2])
-// .addTo(map);
+// Create a default Marker and add it to the map.
 
-map.on('load', () => {
-    // Add a new source from our GeoJSON data and
-    // set the 'cluster' option to true. GL-JS will
-    // add the point_count property to your source data.
-    map.addSource('earthquakes', {
-    type: 'geojson',
-    // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
-    // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-    data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
-    cluster: true,
-    clusterMaxZoom: 14, // Max zoom to cluster points on
-    clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
-    });
-     
-    map.addLayer({
-    id: 'clusters',
-    type: 'circle',
-    source: 'earthquakes',
-    filter: ['has', 'point_count'],
-    paint: {
-    // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
-    // with three steps to implement three types of circles:
-    //   * Blue, 20px circles when point count is less than 100
-    //   * Yellow, 30px circles when point count is between 100 and 750
-    //   * Pink, 40px circles when point count is greater than or equal to 750
-    'circle-color': [
-    'step',
-    ['get', 'point_count'],
-    '#51bbd6',
-    100,
-    '#f1f075',
-    750,
-    '#f28cb1'
-    ],
-    'circle-radius': [
-    'step',
-    ['get', 'point_count'],
-    20,
-    100,
-    30,
-    750,
-    40
-    ]
-    }
-    });
-     
-    map.addLayer({
-    id: 'cluster-count',
-    type: 'symbol',
-    source: 'earthquakes',
-    filter: ['has', 'point_count'],
-    layout: {
-    'text-field': '{point_count_abbreviated}',
-    'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
-    'text-size': 12
-    }
-    });
-     
-    map.addLayer({
-    id: 'unclustered-point',
-    type: 'circle',
-    source: 'earthquakes',
-    filter: ['!', ['has', 'point_count']],
-    paint: {
-    'circle-color': '#11b4da',
-    'circle-radius': 4,
-    'circle-stroke-width': 1,
-    'circle-stroke-color': '#fff'
-    }
-    });
-     
-    // inspect a cluster on click
-    map.on('click', 'clusters', (e) => {
-    const features = map.queryRenderedFeatures(e.point, {
-    layers: ['clusters']
-    });
-    const clusterId = features[0].properties.cluster_id;
-    map.getSource('earthquakes').getClusterExpansionZoom(
-    clusterId,
-    (err, zoom) => {
-    if (err) return;
-     
-    map.easeTo({
-    center: features[0].geometry.coordinates,
-    zoom: zoom
-    });
-    }
-    );
-    });
-     
-    // When a click event occurs on a feature in
-    // the unclustered-point layer, open a popup at
-    // the location of the feature, with
-    // description HTML from its properties.
-    map.on('click', 'unclustered-point', (e) => {
-    const coordinates = e.features[0].geometry.coordinates.slice();
-    const mag = e.features[0].properties.mag;
-    const tsunami =
-    e.features[0].properties.tsunami === 1 ? 'yes' : 'no';
-     
-    // Ensure that if the map is zoomed out such that
-    // multiple copies of the feature are visible, the
-    // popup appears over the copy being pointed to.
-    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-    }
-     
-    new mapboxgl.Popup()
-    .setLngLat(coordinates)
-    .setHTML(
-    `magnitude: ${mag}<br>Was there a tsunami?: ${tsunami}`
-    )
+  for(let i = 0; i < cityGeoJSON.length; i++){
+    let marker1 = new mapboxgl.Marker({ color: 'rgb(20, 200, 175)', rotation: 45 })
+    .setLngLat(cityGeoJSON[i])
     .addTo(map);
-    });
-     
-    map.on('mouseenter', 'clusters', () => {
-    map.getCanvas().style.cursor = 'pointer';
-    });
-    map.on('mouseleave', 'clusters', () => {
-    map.getCanvas().style.cursor = '';
-    });
-    });
+  }
+}
